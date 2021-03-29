7be53484c4d697206a1315e4fdf1e6d4bd49b6fa
wggreen <wggreen.nss@gmail.com>
2021-03-29 10:17:53 -0500

    about to add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
+<template></template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { mapActions } from "vuex";
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  methods: {
+    ...mapActions(["getRandomRecipes"]),
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,21 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    searchResults: []
+  },
+  mutations: {
+    setSearchResults(state, payload) {
+      state.searchResults = payload;
+    }
+  },
+  actions: {
+    async getRandomRecipes({ commit }) {
+      const url = `https://api.spoonacular.com/recipes/random?number=3&apiKey=${process.env.VUE_APP_SPOONACULAR_KEY}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      commit("setSearchResults", data.recipes);
+    },
+  },
   modules: {},
 });
